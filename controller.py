d508e5115103e89becf578a732d3c0af4a36813f
seoset <110677435+seoset@users.noreply.github.com>
2022-10-21 20:05:05 +0300

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,11 @@
+from User_Interface import user_choice
+from  process import data_processor
+from  work_fileimport *
+import telegram_bot_commands as tbc
+
+data = 'processing_model.csv'
+user_list = data_processor(read_file(data), user_choice())
+print(user_list)
+tbc.id_command(update, context)
+
+write_file(user_list, mod='a')
